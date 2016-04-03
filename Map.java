165bf10be9880f12a7bbf0221550ac2d202b654d
Alec Wills <awills@pizzahutte.net>
2016-04-02 20:01:33 -0400

    Randomly places pits on map.  Number of pits is based on the number of map squares / 5.

--- Map.java
+++ Map.java
@@ -8,25 +8,52 @@ public class Map {
     private int width;
     private int height;
 
+    Square[][] wumpusMap;
+
     //Constructor
     Map(){
 
-        Square[][] wumpusMap = new Square[4][4];
+        wumpusMap = new Square[4][4];
         width = 4;
         height = 4;
+
+        seedMap();
 
     }
 
     //Construct map with specific dimensions
     Map(int width, int height){
 
-        Square[][] wumpusMap = new Square[width][height];
+        wumpusMap = new Square[width][height];
         this.width = width;
         this.height = height;
 
+        seedMap();
     }
 
     private void seedMap(){
+
+        int pitCount = (width * height)/5; //Determine a number of pits to put into the game, trying /5 based on book map
+        int x, y;
+
+        for(int i = 0; i < height; i++) {
+
+            for (int j = 0; j < width; j++) {
+
+                wumpusMap[j][i] = new Square();
+
+            }
+        }
+
+        //Assign pits
+        for(int i = 0; i < pitCount; i++)   {
+
+            x = (int)(Math.random() * width);
+            y = (int)(Math.random() * height);
+
+            wumpusMap[x][y].hasPit = true;
+            wumpusMap[x][y].mapChar = 'P';  //TODO remove after testing
+        }
 
     }
 
@@ -36,7 +63,7 @@ public class Map {
 
             for(int j = 0; j < width; j++){
 
-                System.out.print("X ");
+                System.out.print(wumpusMap[j][i].mapChar + " ");
 
             }
 
@@ -54,6 +81,7 @@ public class Map {
         boolean hasBreeze = false;
         boolean hasPit = false;
 
+        char mapChar = 'X';
     }
 
 
