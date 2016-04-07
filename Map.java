64eeaae875974f2020c4eccbb41f0f8ef02e2d57
Eunice Talton <echoe88@gmail.com>
2016-04-07 15:51:03 +0000

    Map.java edited online with Bitbucket

--- Map.java
+++ Map.java
@@ -1,23 +1,16 @@
-//package sample;
-
-import javafx.scene.layout.GridPane;
-
 /**
- * Created by awills on 4/2/16.
+ * Created by Alexander Wills on 4/2/16.
  */
 
 //Generate map to play wumpusworld game on
-public class Map extends GridPane
-{
+public class Map {
 
+    Square[][] wumpusMap;
     private int width;
     private int height;
 
-    Square[][] wumpusMap;
-
-
     //Constructor
-    Map(){
+    Map() {
 
         wumpusMap = new Square[4][4];
         width = 4;
@@ -28,7 +21,7 @@ public class Map extends GridPane
     }
 
     //Construct map with specific dimensions
-    Map(int width, int height){
+    Map(int width, int height) {
 
         wumpusMap = new Square[width][height];
         this.width = width;
@@ -37,67 +30,65 @@ public class Map extends GridPane
         seedMap();
     }
 
-    private void seedMap(){
+    private void seedMap() {
 
-        int pitCount = (width * height)/5; //Determine a number of pits to put into the game, trying /5 based on book map
+        int pitCount = (width * height) / 5; //Determine a number of pits to put into the game, trying /5 based on book map
         int x, y;
 
-        for(int i = 0; i < height; i++) {
+        for (int i = 0; i < height; i++) {
 
             for (int j = 0; j < width; j++) {
 
-                wumpusMap[i][j] = new Square();
-                this.add(wumpusMap[i][j], i, j);
+                wumpusMap[j][i] = new Square();
 
             }
         }
 
         //Assign pits
-        for(int i = 0; i < pitCount; i++)   {
+        for (int i = 0; i < pitCount; i++) {
 
-            // TODO(Andrew): exclude protagonist starting point
-            x = (int)(Math.random() * width);
-            y = (int)(Math.random() * height);
+            x = (int) (Math.random() * width);
+            y = (int) (Math.random() * height);
 
             //Assign breeze first so it doesn't write over pit
             //TODO still writing over it, need to handle this in a separate loop, maybe in a method in the Square class to set its char appropriately once all stats are assigned
-            if(x != width - 1) {
+            if (x != width - 1) {
                 wumpusMap[x + 1][y].hasBreeze = true;
-                wumpusMap[x + 1][y].setMapChar('B');
+                wumpusMap[x + 1][y].mapChar = 'B';
 
             }
 
-            if(x != 0) {
+            if (x != 0) {
                 wumpusMap[x - 1][y].hasBreeze = true;
-                wumpusMap[x - 1][y].setMapChar('B');
+                wumpusMap[x - 1][y].mapChar = 'B';
 
             }
 
-            if(y != height - 1) {
+            if (y != height - 1) {
                 wumpusMap[x][y + 1].hasBreeze = true;
-                wumpusMap[x][y + 1].setMapChar('B');
+                wumpusMap[x][y + 1].mapChar = 'B';
 
             }
 
-            if(y != 0) {
+            if (y != 0) {
                 wumpusMap[x][y - 1].hasBreeze = true;
-                wumpusMap[x][y - 1].setMapChar('B');
+                wumpusMap[x][y - 1].mapChar = 'B';
 
             }
 
             //Assign pit
             wumpusMap[x][y].hasPit = true;
-            wumpusMap[x][y].setMapChar('P');
+            wumpusMap[x][y].mapChar = 'P';
 
         }
 
     }
 
-    public void printMap(){
+    public void printMap() {
 
-        for(int i = 0; i < height; i++){
+        for (int i = 0; i < height; i++) {
 
-            for(int j = 0; j < width; j++){
+            for (int j = 0; j < width; j++) {
 
                 System.out.print(wumpusMap[j][i].mapChar + " ");
 
@@ -109,7 +100,17 @@ public class Map extends GridPane
 
     }
 
+    private class Square {
 
+        boolean hasWumpus = false;
+        boolean hasGold = false;
+        boolean hasStench = false;
+        boolean hasBreeze = false;
+        boolean hasPit = false;
+        boolean isStart = false;
+
+        char mapChar = 'X';
+    }
 
 
 }
